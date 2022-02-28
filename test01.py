a94a92704653adc1a971eb1065365fdee2aae3bb
Konrad <84790331+konradjasitczak@users.noreply.github.com>
2022-02-28 13:47:49 +0100

    Update test01.py

--- test01.py
+++ test01.py
@@ -1,23 +1,24 @@
 import pytest
 import logging
 
-logging.basicConfig(level=logging.INFO)
+log=logging.basicConfig(level=logging.INFO)
 
 class Test01Android():
     @pytest.mark.parametrize('os', ['android'])
-    def test_01(self):
-        logging.info("test_01 method")
-
+    def test_01(self, os):
+        log.info(f"mark value is {os}")
+        #log.info("mark value is {s},{d}".format(s=os, d="12"))
+        #log.info("mark value is %s" % os)
     @classmethod
     def setup_class(cls):
-        logging.info("setup_class")
+        log.info("setup_class")
 
     @classmethod
     def teardown_class(cls):
-        logging.info("teardown_class")
+        log.info("teardown_class")
 
-    def setup_method(self, method):
-        logging.info("setup_method")
+    def setup_method(self):
+        log.info("setup_method")
 
-    def teardown_method(self, method):
-        logging.info("teardown_method")
+    def teardown_method(self):
+        log.info("teardown_method")
