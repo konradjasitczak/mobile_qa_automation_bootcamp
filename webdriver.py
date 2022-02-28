be5b388a9f80e9a138570a060874ed899d9e7f66
Konrad <84790331+konradjasitczak@users.noreply.github.com>
2022-02-28 13:57:11 +0100

    Update webdriver.py

--- webdriver.py
+++ webdriver.py
@@ -7,16 +7,16 @@ class WebCommon:
         self.driver = None
 
     def init_driver(self):
-        self.driver = self.get_driver()
-
-    def get_driver(self):
         desired_caps = {
             "platformName": "",
             "appPackage": "",
             "appActivity": "",
         }
+        
+        self.driver = webdriver.Remote('localhost', desired_caps)
 
-        return webdriver.Remote('localhost', desired_caps)
+    def get_driver(self):
+        return self.driver
 
     def close_driver(self):
         self.driver.quit()
