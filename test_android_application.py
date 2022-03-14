f5d2322dfca7f4bb42256e4ad2b9ce55193a72c1
konradjasitczak <konrad@qtf.io>
2022-03-14 13:00:40 +0100

    code upgrade

--- test_android_application.py
+++ test_android_application.py
@@ -2,14 +2,8 @@ import pytest
 import logging
 import time
 
-from selenium.webdriver.common.actions import interaction
-from selenium.webdriver.common.actions.action_builder import ActionBuilder
-from selenium.webdriver.common.actions.pointer_input import PointerInput
-
 from webdriver import WebCommon
 from appium.webdriver.webdriver import AppiumBy
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver import ActionChains
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
@@ -23,12 +17,15 @@ class Test01Android:
     def setup_method(self, method):
         log.info("setup_method")
         log.info(method.__name__)
-        # if
         apk_name = "theapp"
-        # else:
-        #     apk_name = "filemanager"
         self.webcommon = WebCommon(apk_name)
         self.driver = self.webcommon.get_driver()
+        apk_filemanager = "filemanager"
+        self.webcommon2 = WebCommon(apk_filemanager)
+        self.filemanager = self.webcommon2.get_driver()
+
+    def get_element_by_text(self, text):
+        return self.driver.find_element_by_xpath(f"//*[@text=\"{text}\"]")
 
     def teardown_method(self):
         log.info("teardown_method")
@@ -59,15 +56,10 @@ class Test01Android:
         assert len(list_elements) == 7, "list length is not 7"
 
     def test_05_text(self):
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'List Demo').click()
-        #self.driver.find_elements_by_xpath(f"//*[@text='List Demo']")
+        self.get_element_by_text("List Demo").click()
         self.driver.implicitly_wait(1)
-        try:
-            self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Cirrus')
-        except NoSuchElementException:
-            return False
-        return True
-        #self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Cirrus').size != 0
+        log.info("Check if Cirrus element exists")
+        assert self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Cirrus')
 
     def test_06_send_keys(self):
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Echo Box').click()
@@ -77,7 +69,7 @@ class Test01Android:
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'messageSaveBtn').click()
         self.driver.implicitly_wait(1)
         log.info("Check 'Hello World' is printed")
-        return self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Hello World')
+        assert self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Hello World')
 
     def test_07_wait(self):
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'List Demo').click()
@@ -89,16 +81,12 @@ class Test01Android:
             if test == 10:
                 break
             test -= 1
+        log.info("Check Cirrus element is present")
+        assert self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Cirrus')
 
     def test_08_scroll(self):
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'List Demo').click()
         self.driver.implicitly_wait(1)
-        actions = ActionChains(self.driver)
-        actions.w3c_actions = ActionBuilder(self.driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch"))
-        actions.w3c_actions.pointer_action.move_to_location(570, 1344)
-        actions.w3c_actions.pointer_action.pointer_down()
-        actions.w3c_actions.pointer_action.move_to_location(579, 512)
-        actions.w3c_actions.pointer_action.release()
-        actions.perform()
+        self.driver.swipe(0, 1344, 0, 512)
         log.info("Check last item is available")
-        return self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Stratus')
+        assert self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Stratus')
